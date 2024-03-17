96102e047c94c828539482f0512036a73d4b4d8f
drey7925 <drey@dreyclimbs.rocks>
2024-03-16 22:25:58 -0400

    deferred routines, not really well tested yet

--- perovskite_game_api/src/default_game/mapgen/mod.rs
+++ perovskite_game_api/src/default_game/mapgen/mod.rs
@@ -238,6 +238,7 @@ struct DefaultMapgen {
     tall_grass_density_noise: noise::Billow<noise::SuperSimplex>,
 
     karst_noise: karst::KarstGenerator,
+    rail_testonly: BlockTypeHandle,
 
     biome_noise: BiomeNoise,
     macrobiome_noise: MacrobiomeNoise,
@@ -365,6 +366,23 @@ impl MapgenInterface for DefaultMapgen {
             }
         }
         self.generate_vegetation(chunk_coord, chunk, &height_map, &macrobiome_map, &biome_map);
+        for x in 0..16 {
+            for z in 0..16 {
+                let xg = 16 * chunk_coord.x + (x as i32);
+
+                if xg == 320000 && chunk_coord.y == 3 {
+                    // todo: generate rails
+                    chunk.set_block(ChunkOffset { x, y: 0, z }, self.rail_testonly, None);
+                }
+
+                if xg == 960000 && chunk_coord.y == 0 {
+                    // todo: generate rails
+                    chunk.set_block(ChunkOffset { x, y: 4, z }, self.rail_testonly, None);
+                    chunk.set_block(ChunkOffset { x, y: 5, z }, self.air, None);
+                    chunk.set_block(ChunkOffset { x, y: 6, z }, self.air, None);
+                }
+            }
+        }
     }
 }
 
@@ -792,5 +810,7 @@ pub(crate) fn build_mapgen(
             })
             .collect(),
         seed,
+
+        rail_testonly: blocks.get_by_name("carts:rail").expect("rail"),
     })
 }
