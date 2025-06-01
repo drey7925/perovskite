30c4655f36fd2871c9adc02da1572387c32bf946
drey <drey@dreyclimbs.rocks>
2025-05-31 23:13:15 -0400

    Fix shutdown delay waiting for timer shards; with very long-running timers shutdown could take a very long time under the previous behavior
    (cherry picked from commit 5bb2751e0e89d5586f351f4eb03f0c27e930b91c)

--- perovskite_server/src/game_state/game_map.rs
+++ perovskite_server/src/game_state/game_map.rs
@@ -2188,7 +2188,7 @@ impl MapCacheCleanup {
                     tokio::task::block_in_place(|| self.do_cleanup())?;
                 }
                 _ = self.cancellation.cancelled() => {
-                    info!("Map cache cleanup thread shutting down");
+                    info!("Map cache cleanup thread shutting down for shard ID {}", self.shard_id);
                     break;
                 }
             }
@@ -2256,7 +2256,7 @@ impl GameMapWriteback {
             tokio::task::block_in_place(|| self.do_writebacks(writebacks))?;
         }
 
-        info!("Map writeback exiting");
+        info!("Map writeback exiting for shard ID {}", self.shard_id);
         Ok(())
     }
 
@@ -2318,7 +2318,7 @@ impl GameMapWriteback {
                 }
             },
             _ = self.cancellation.cancelled() => {
-                log::info!("Map writeback detected cancellation");
+                log::info!("Map writeback detected cancellation while gathering");
                 false
             }
         };
@@ -2859,6 +2859,9 @@ impl GameMapTimer {
 
                     tokio::task::block_in_place(|| self.delegate_locking_path(coarse_shard, fine_shard, fine_shards_per_coarse, game_state.clone(), &block_types, &mut shard_state))?;
                     shard_state.timer_state.prev_tick_time = current_tick_start;
+                },
+                _ = self.cancellation.cancelled() => {
+                    break;
                 }
             }
         }
